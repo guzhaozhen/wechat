6533fcbd8b30bbe1835c030710247d0ca60a833f
guzhaozhen <1123370957@qq.com>
2020-07-28 18:49:54 +0800

    update .gitignore

--- app/Http/Controllers/WeXin/IndexController.php
+++ app/Http/Controllers/WeXin/IndexController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Log;
 use App\Models\Media;
+use App\Models\Type;
 class IndexController extends Controller
 {
     //开通微信公众平台测试账号
@@ -38,32 +39,37 @@ class IndexController extends Controller
     }
 
     //定时任务 定时删除
-    public function deletepast(){
-       Log::info('这是一个测试任务');
-       $time = time();
-       $pageSize = config('app.pageSize');
-        $mediaInfo = Media::join('type','media.t_id','=','type.t_id')
-            ->Orderby('m_id','desc')->paginate($pageSize);
-        $ids = $url = [];
-        foreach($mediaInfo as $key=>$value){
-            if($time-$value->addtime>3*24*60*60){
-                $urls[$key] = $value->m_img;
-                $ids[$key] = $value->m_id;
-            }
-        }
-       // dd($ids);
-       // 文件删除 (阿里云 如果是永久素材记得删除微信服务器的)
-        foreach($urls as $v){
-            $path = storage_path('app/'.$v);
-            if(file_exists($path)){
-                unlink($path);
-            }
-        }
+//    public function deletepast(){
+//       Log::info('这是一个测试任务');
+//       $time = time();
+//       $pageSize = config('app.pageSize');
+//        $mediaInfo = Media::join('type','media.t_id','=','type.t_id')
+//            ->Orderby('m_id','desc')->paginate($pageSize);
+//        $ids = $url = [];
+//        foreach($mediaInfo as $key=>$value){
+//            if($time-$value->addtime>3*24*60*60){
+//                $urls[$key] = $value->m_img;
+//                $ids[$key] = $value->m_id;
+//            }
+//        }
+//       // dd($ids);
+//       // 文件删除 (阿里云 如果是永久素材记得删除微信服务器的)
+//        foreach($urls as $v){
+//            $path = storage_path('app/'.$v);
+//            if(file_exists($path)){
+//                unlink($path);
+//            }
+//        }
 //        删除
-        $res = Media::destory($ids);
-        if($res){
-            Log::info('删除素材成功主键ID为:'.implode(','.$ids));
-        }
+//        $res = Media::destory($ids);
+//        if($res){
+//            Log::info('删除素材成功主键ID为:'.implode(','.$ids));
+//        }
+//
+//    }
 
+    public function create(){
+        $typeInfo = Type::get();
+        return view('wexin.create',['typeInfo'=>$$typeInfo]);
     }
 }
